93486e48c427be4f64c5b2d6d5c7f11c4c7cbeea
Yajahira <yari@yvelazquez.com>
2022-09-21 19:48:26 -0400

    created a readme and add and all task activities

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a mobile app designed to allow users to add tasks and view all added tasks.
+
+9/21/2022
+
+![Home Page](Images/homescreen.png)
+![Add Task](Images/addtask.png)
+![All Task](Images/alltask.png)
+
--- app/src/main/java/com/dt_cs/taskmaster/MainActivity.java
+++ app/src/main/java/com/dt_cs/taskmaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.dt_cs.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpTaskBtns();
+    }
+
+    private void setUpTaskBtns(){
+        Button addTaskBtn = findViewById(R.id.MainActivityAddTaskBtn);
+        addTaskBtn.setOnClickListener(view -> {
+            Intent goToAddTask = new Intent(MainActivity.this, AddTask.class);
+            startActivity(goToAddTask);
+        });
+
+        Button allTasksBtn = findViewById(R.id.MainActivityAllTaskBtn);
+        allTasksBtn.setOnClickListener(view -> {
+            Intent goToAllTasks = new Intent(MainActivity.this, AllTasks.class);
+            startActivity(goToAllTasks);
+        });
     }
 }
